a8607bba79e22d9f467bf65656479a3d648bbf44
LiChunqiang <lcqforwork@163.com>
2020-04-08 09:26:01 +0800

    日报自动生成器  Ver.0.0.2 优化：仅获取当日git提交记录

--- WebSpider.py
+++ WebSpider.py
@@ -1,7 +1,8 @@
+import re
+from datetime import datetime, timedelta, timezone
+
 import requests as req
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta, timezone
-import re
 
 gitUserName = input("请输入gitlab用户名：")
 offset = 0
@@ -11,8 +12,11 @@ today = today.replace(today.year, today.month, today.day, 0, 0, 0, 0)
 
 print("今日工作:")
 
+workLog = ''
 srcHtmlText = ''
 utcTime = None
+times = None
+timeTag = None
 
 while utcTime is None or utcTime > today:
     url = "http://172.20.20.81/" + gitUserName + ".atom?limit=20&offset=" + str(offset)
@@ -23,19 +27,31 @@ while utcTime is None or utcTime > today:
     utcTime = datetime.strptime(times[len(times) - 1].string, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
     offset += 20
 
-soup = BeautifulSoup(srcHtmlText, 'lxml')
-data = soup.find_all('p', dir="auto")
-regex = re.compile("Merge.*")
-logSet = set()
-workLog = ''
-count = 1
-for comment in data:
-    if regex.match(comment.string):
-        continue
-    if comment.string in logSet:
-        continue
-    workLog += str(count) + "." + comment.string + "\n"
-    count = count + 1
-    logSet.add(comment.string)
+#仅截取当日git提交记录
+for index in range(len(times) - 1, -1, -1):
+    utcTime = datetime.strptime(times[index].string, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
+    if utcTime > today:
+        timeTag = times[index].string
+        break
+
+if timeTag is None:
+    srcHtmlText = ''
+else:
+    srcHtmlText = srcHtmlText[0:srcHtmlText.index(timeTag)]
+    soup = BeautifulSoup(srcHtmlText, 'lxml')
+    data = soup.find_all('p', dir="auto")
+    regex = re.compile("Merge.*")
+    logSet = set()
+    count = 1
+    for comment in data:
+        # 过滤Merge记录
+        if regex.match(comment.string):
+            continue
+        #过滤重复commit记录
+        if comment.string in logSet:
+            continue
+        workLog += str(count) + "." + comment.string + "\n"
+        count = count + 1
+        logSet.add(comment.string)
 
 print(workLog)
